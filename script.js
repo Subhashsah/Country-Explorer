d4478a115184cdb8bdf484fc6de2e4313737cc0c
Subhash <prasadsah500@gmail.com>
2024-10-03 01:03:46 +0530

    Added Search Bar, Filter and DarkMode

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const countriesCard= document.querySelector('.countries-card')
+const filterByRegion = document.querySelector('.filter-by-region')
+const inputFiled=document.querySelector('.inputFiled')
+const themechange=document.querySelector('.theme-change')
+
+let allCountries;
 
 fetch('https://restcountries.com/v3.1/all')
 .then((res)=>res.json())
 .then((data)=>{
+    renderCountries(data)
+    allCountries=data
+})
+    
+filterByRegion.addEventListener('change',(e)=>{
+fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+.then((res)=>res.json())
+.then(renderCountries)
+})
+
+inputFiled.addEventListener('input',(e)=>{
+    const result=allCountries.filter((Country)=>Country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+    renderCountries(result)
+})
+
+
+
+function renderCountries(data){
+    countriesCard.innerHTML='';
     data.forEach((Country)=>{
         const countryCard = document.createElement('a')
         countryCard.classList.add('country-card')
@@ -16,9 +40,8 @@ fetch('https://restcountries.com/v3.1/all')
         </div>`
         countriesCard.appendChild(countryCard)
     })
-        
-    })
+}
 
-
-
-
+themechange.addEventListener('click',()=>{
+    document.body.classList.toggle('dark')
+})
